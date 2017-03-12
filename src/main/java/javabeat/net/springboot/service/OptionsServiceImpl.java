2ac2f804b1427469178b22f75ebe8268ca83b8b7
Elin Dahlin <elin-dahlin@hotmail.com>
2017-03-12 01:25:48 +0100

    Option source keys added

--- src/main/java/javabeat/net/springboot/service/OptionsServiceImpl.java
+++ src/main/java/javabeat/net/springboot/service/OptionsServiceImpl.java
@@ -1,8 +1,10 @@
 package javabeat.net.springboot.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -32,15 +34,69 @@ public class OptionsServiceImpl implements OptionsService {
 		Collection<Option> options = new ArrayList<>();
 		switch (category) {
 		case COFFEE:
-			String type = "cafe";
-			options = googleGateway.findOptionsByType(type, lat, lon, radius);
+			options = googleGateway.findOptionsByType("cafe", lat, lon, radius);
+			break;
+		case CINEMA:
+			options = googleGateway.findOptionsByType("movie_theater", lat, lon, radius);
+			break;
+		case MUSEUM:
+			options = googleGateway.findOptionsByType("museum", lat, lon, radius);
+			break;
+		case THEATER:
+			options = googleGateway.findOptionsByText("theatre", lat, lon, radius);
+			break;
+		case PAINTBALL:
+			options = googleGateway.findOptionsByText("paintball", lat, lon, radius);
+			options.stream()
+				.filter(option -> !Arrays.asList(option.getTypes()).contains("store"))
+				.collect(Collectors.toList());
+			break;
+		case LASERTAG:
+			options = googleGateway.findOptionsByText("laser+tag", lat, lon, radius);
+			break;
+		case PARK:
+			options = googleGateway.findOptionsByText("park", lat, lon, radius);
+			break;
+		case RESTAURANT:
+			options = googleGateway.findOptionsByType("restaurant", lat, lon, radius);
 			break;
 		case ESCAPEROOM:
-			String text = "escape+room";
-			options = googleGateway.findOptionsByText(text, lat, lon, radius);
+			options = googleGateway.findOptionsByText("escape+room", lat, lon, radius);
+			break;
+		case SPA:
+			options = googleGateway.findOptionsByType("spa", lat, lon, radius);
+			break;
+		case ICESKATING:
+			options = googleGateway.findOptionsByText("ice+skating", lat, lon, radius);
+			options.stream()
+				.filter(option -> !Arrays.asList(option.getTypes()).contains("store"))
+				.collect(Collectors.toList());
+			break;
+		case BOWLING:
+			options = googleGateway.findOptionsByType("bowling_alley", lat, lon, radius);
+			break;
+		case CLIMBING:
+			options = googleGateway.findOptionsByText("climbing", lat, lon, radius);
+			break;
+		case BILLIARDS:
+			options = googleGateway.findOptionsByText("billiard", lat, lon, radius);
+			options.stream()
+				.filter(option -> !Arrays.asList(option.getTypes()).contains("store"))
+				.collect(Collectors.toList());
+			break;
+		case SWIMMING:
+			options = googleGateway.findOptionsByText("swimming", lat, lon, radius);
+			options.stream()
+				.filter(option -> !Arrays.asList(option.getTypes()).contains("store"))
+				.collect(Collectors.toList());
+			break;
+		case SHOPPING:
+			options = googleGateway.findOptionsByType("shopping_mall", lat, lon, radius);
+			options.addAll(googleGateway.findOptionsByType("clothing_store", lat, lon, radius));
+			options.addAll(googleGateway.findOptionsByType("shoe_store", lat, lon, radius));
 			break;
 		default:
-			LOGGER.warn("category option search method undefined");
+			LOGGER.warn("category option search method undefined for type: " + category);
 			options = Collections.emptyList();
 			break;
 		}
