cca09dbb7a0188b10d71d4dfca65cf3a3070a2b2
Elin Dahlin <elin-dahlin@hotmail.com>
2017-03-16 14:02:44 +0100

    Set base path for heroku

--- src/main/webapp/js/ajax.js
+++ src/main/webapp/js/ajax.js
@@ -1,6 +1,7 @@
 function ajaxGet(localUrl) {
-    var url = "http://localhost:9090";
-    //var url = "https://mayfly-c-plus-plus-plus.herokuapp.com";
+    // set base path depending on execution environment
+    //var url = "http://localhost:9090";
+    var url = "https://mayfly-c-plus-plus-plus.herokuapp.com";
     return $.ajax({
         url: url + localUrl, 
         type: 'GET',
